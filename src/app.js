1e34e2eccb1de92db5575a576f526c7d35104802
shubhankjarvis <shubhank.thakur@jarvis.consulting>
2025-12-18 12:59:13 +0530

    error handling with middleware

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const { userAuth , adminAuth} = require("./middlewares/auth");
+const { userAuth, adminAuth } = require("./middlewares/auth");
 
 const app = express();
 const port = 3000;
 
+
 app.get("/user", userAuth, (req, res) => {
-    res.status(200).send("User Authenticated")
+    try {
+        res.status(200).send("User Authenticated")
+    } catch (err) {
+        res.status(500).send("Something not good : error")
+    }
 })
 
 app.post("/admin", adminAuth, (req, res) => {
     res.status(200).send("Data Successfully saved to db")
 })
 
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong");
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running at port : ${port}`);
