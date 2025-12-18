e03e88268b5a423d93f1dacb90f0d43d7bbe6ebc
shubhankjarvis <shubhank.thakur@jarvis.consulting>
2025-12-18 23:02:34 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-const port = 7777; 
+const port = 7777;
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+    // creating a new instance of User model
+    const user = new User({
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId: "viratkohli18@gmail.com",
+        password: "virat@123"
+    });
+
+    try {
+        await user.save();
+        res.status(200).send("User added successully")
+    } catch (err) {
+        res.status(400).send("Error saving user : ", err.message);
+    }
+})
 
 connectDB()
     .then(() => {
@@ -14,4 +32,4 @@ connectDB()
         console.error("Database cannot br connected!!...")
     });
 
-     
+
